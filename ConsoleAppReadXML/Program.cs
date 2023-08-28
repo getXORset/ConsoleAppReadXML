b88b4a6c8855607a6a4ceb4a46306685e70bf383
Karin Jonsson <tamburfilm@gmail.com>
2023-08-28 15:01:24 +0200

    Added try-catch

--- ConsoleAppReadXML/Program.cs
+++ ConsoleAppReadXML/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System.ComponentModel.Design.Serialization;
 using System.Xml.Linq;
 
-//Loading the XML-file by using the class XElement (LINQ to XML)
-XElement targetFromFile = XElement.Load(@"sma_gentext.xml");
+try
+{
+    //Loading the XML-file by using the class XElement (LINQ to XML)
+    XElement targetFromFile = XElement.Load(@"sma_gentext.xml");
 
-//Picking the unit with id=42007
-XElement trans_unit = targetFromFile.Descendants().First(node => (string)node.Attribute("id") == "42007");
+    //Picking the unit with id=42007
+    XElement trans_unit = targetFromFile.Descendants().First(node => (string)node.Attribute("id") == "42007");
 
-//Writing the value of the target element to a file
-TextWriter toTextfile = new StreamWriter("targetValue.txt");
-toTextfile.WriteLine(trans_unit.Element("target").Value);
-toTextfile.Close();
+    //Writing the value of the target element to a file
+    TextWriter toTextfile = new StreamWriter("targetValue.txt");
+    toTextfile.WriteLine(trans_unit.Element("target").Value);
+    toTextfile.Close();
+}
+catch (Exception)
+{
+    Console.WriteLine("Something went wrong!");
+}
+
