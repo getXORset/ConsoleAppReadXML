8a869260fde9501ec83210118477d905b72db58a
Karin Jonsson <tamburfilm@gmail.com>
2023-08-28 14:19:52 +0200

    Add project files.

---
+++ ConsoleAppReadXML/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.Design.Serialization;
+using System.Xml.Linq;
+
+//Loading the XML-file by using the class XElement (LINQ to XML)
+XElement targetFromFile = XElement.Load(@"sma_gentext.xml");
+
+//Picking the unit with id=42007
+XElement trans_unit = targetFromFile.Descendants().First(node => (string)node.Attribute("id") == "42007");
+
+//Writing the value of the target element to a file
+TextWriter toTextfile = new StreamWriter("targetValue.txt");
+toTextfile.WriteLine(trans_unit.Element("target").Value);
+toTextfile.Close();
